56f90be02bc3fc6fae4c8aed930a6dcf64a454fc
J-Schild <jschild@uni-bonn.de>
2021-06-30 12:01:29 +0200

    test

---
+++ seminar_1_test_R.R
@@ -0,0 +1,30 @@
+install.packages("chillR")
+library (chillR)
+?chillR
+weather <- fix_weather(
+  KA_weather[which(KA_weather$Year>2004&!(KA_weather$Year==2005&KA_weather$Month<6)),])
+version
+
+library(tidyverse)
+install.packages("tidyverse")
+library(tidyverse)
+browseVignettes("tidyverse")
+# ?read.csv
+read.csv("participants_data.csv")
+
+
+# dplyr ####
+
+library(tidyverse)
+
+library(dplyr)
+
+install.packages("dplyr")
+
+install.packages("dplyr")
+
+library(dplyr)
+
+library(ggplot2)
+
+?diamonds
